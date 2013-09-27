7fc3076c392c13552b80c34ac93732567582bc82
mehmetatas <mehmetatasm@gmail.com>
2013-09-27 21:45:03 +0300

    kurcalama 3

--- framework/src/com/blox/framework/v0/impl/libgdx/GdxGame.java
+++ framework/src/com/blox/framework/v0/impl/libgdx/GdxGame.java
@@ -7,7 +7,6 @@ import org.w3c.dom.Document;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL10;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
@@ -40,12 +39,12 @@ public class GdxGame implements ApplicationListener {
 	public void create() {
 		spriteBatch = new SpriteBatch();
 		shapeRenderer = new ShapeRenderer();
-		
+
 		initGdx();
 
 		Game.initialize(getGameXml());
 		Game.getInputManager().activate();
-		
+
 		game = (IGame) Utils.createInstance(GameMetadata.getGameClass());
 		game.init();
 	}
@@ -53,9 +52,9 @@ public class GdxGame implements ApplicationListener {
 	@Override
 	public void render() {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
-		//Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT | (Gdx.graphics.getBufferFormat().coverageSampling?GL20.GL_COVERAGE_BUFFER_BIT_NV:0));
-		//Gdx.gl.glEnable(GL10.GL_BLEND);
-        Gdx.gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+		Gdx.gl.glEnable(GL10.GL_BLEND);
+		Gdx.gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
 
 		game.update();
 
