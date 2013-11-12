869777dd910fd4b850e515bb4145ada6c1af0038
mehmetatas <mehmetatasm@gmail.com>
2013-11-12 23:21:27 +0200

    Revert "Atmosphere IOS"
    This reverts commit 075cad631c6453c30293083332f6e0584527aa74.

--- ichigu/src/com/turpgames/ichigu/view/ChatScreen.java
+++ ichigu/src/com/turpgames/ichigu/view/ChatScreen.java
@@ -1,35 +1,18 @@
 package com.turpgames.ichigu.view;
 
 import java.io.IOException;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.Date;
-import java.util.List;
 
 import org.atmosphere.wasync.ClientFactory;
 import org.atmosphere.wasync.Decoder;
 import org.atmosphere.wasync.Encoder;
 import org.atmosphere.wasync.Event;
 import org.atmosphere.wasync.Function;
-import org.atmosphere.wasync.OptionsBuilder;
 import org.atmosphere.wasync.Request;
 import org.atmosphere.wasync.RequestBuilder;
 import org.atmosphere.wasync.impl.AtmosphereClient;
-import org.atmosphere.wasync.impl.DefaultOptions;
-import org.atmosphere.wasync.impl.DefaultOptionsBuilder;
 import org.codehaus.jackson.map.ObjectMapper;
 
-import com.ning.http.client.AsyncHandler;
-import com.ning.http.client.AsyncHttpClient;
-import com.ning.http.client.AsyncHttpClientConfig;
-import com.ning.http.client.AsyncHttpProvider;
-import com.ning.http.client.HttpResponseBodyPart;
-import com.ning.http.client.HttpResponseHeaders;
-import com.ning.http.client.HttpResponseStatus;
-import com.ning.http.client.ListenableFuture;
-import com.ning.http.client.Response;
-import com.ning.http.client.providers.netty.NettyAsyncHttpProvider;
-import com.ning.http.client.providers.netty.NettyAsyncHttpProviderConfig;
 import com.turpgames.framework.v0.component.IButtonListener;
 import com.turpgames.framework.v0.component.ImageButton;
 import com.turpgames.framework.v0.impl.Text;
@@ -39,65 +22,28 @@ public class ChatScreen extends IchiguScreen {
 	private ImageButton btn;
 	private Text txt;
 
-	private String serverIpAddress = "http://192.168.2.4:8080/AtmosphereServer/chat";
+	private String serverIpAddress = "http://192.168.2.4:8080/AtmosphereServer";
 	private final static ObjectMapper mapper = new ObjectMapper(); 
 
-	private org.atmosphere.wasync.Socket socket = null;
-	
 	@Override
-	public void init() {		
-		final String name = "ios";
-		
+	public void init() {
 		super.init();
-		
 		txt = new Text();
 		txt.setFontScale(0.5f);
-		txt.setVerticalAlignment(Text.VAlignCenter);
-		txt.setHorizontalAlignment(Text.HAlignCenter);
-		txt.setWidth(Game.getScreenWidth() - 200);
-		txt.setHeight(Game.getScreenHeight() - 200);
-		txt.setX(100);
-		txt.setY(100);
+		txt.setVerticalAlignment(Text.VAlignTop);
+		txt.setHorizontalAlignment(Text.HAlignLeft);
+		txt.setWidth(Game.getScreenWidth());
+		txt.setHeight(Game.getScreenHeight());
 
 		btn = new ImageButton(100, 100, "libgdx");
 		btn.getLocation().set((Game.getScreenWidth() - 100) / 2, 110);
 
-		btn.setListener(new IButtonListener() {
-			@Override
-			public void onButtonTapped() {
-				System.out.print("Button Tapped: ");
-				try {
-					socket.fire(new Data(name, new Date().toString()));
-					System.out.println("OK!");
-				}
-				catch (Throwable e) {
-					txt.setText("ERROR 3: " + e.getMessage());
-					e.printStackTrace();
-					System.out.println(e.getMessage());
-				}
-			}
-		});
-		
 		try {
-			URL url = new URL(serverIpAddress);
-			URLConnection conn =  url.openConnection();
-			conn.getInputStream().close();
-			System.out.println("Connection OK: " + conn.getClass().getName());
-		}
-		catch (Throwable t) {
-			System.out.println("Connection FAIL!");
-			t.printStackTrace();
-		}
-
-		try {
-
-			System.out.println("1");
 			AtmosphereClient client = ClientFactory.getDefault().newClient(AtmosphereClient.class);
 
-			System.out.println("2");
 			RequestBuilder request = client.newRequestBuilder()
 					.method(Request.METHOD.GET)
-					.uri(serverIpAddress)
+					.uri(serverIpAddress + "/chat")
 					.trackMessageLength(true)
 					.encoder(new Encoder<Data, String>() {
 						@Override
@@ -135,18 +81,12 @@ public class ChatScreen extends IchiguScreen {
 							}
 						}
 					})
-					;
-			
-			System.out.println("3");
-			DefaultOptionsBuilder options = new DefaultOptionsBuilder(){};
-			options.runtime(new AsyncHttpClient(new NettyAsyncHttpProvider(new AsyncHttpClientConfig.Builder().build())));
-			socket = client.create(new DefaultOptions(options));
-			
-			System.out.println("4");
+					.transport(Request.TRANSPORT.WEBSOCKET);
+
+			final org.atmosphere.wasync.Socket socket = client.create();
 			socket.on("message", new Function<Data>() {
 				@Override
 				public void on(final Data t) {
-					System.out.println("6");
 					Date d = new Date(t.getTime());
 					txt.setText(txt.getText() + "\n" + t.getAuthor() + ": " + t.getMessage());
 				}
@@ -158,12 +98,24 @@ public class ChatScreen extends IchiguScreen {
 				}
 
 			}).open(request.build());
-			
-			
-			System.out.println("5");
+
+			final String name = "taga";
+
+			btn.setListener(new IButtonListener() {
+				@Override
+				public void onButtonTapped() {
+					try {
+						socket.fire(new Data(name, new Date().toString()));
+					}
+					catch (Throwable e) {
+						txt.setText("ERROR 3: " + e.getMessage());
+						e.printStackTrace();
+					}
+				}
+			});
+
 		}
 		catch (Throwable e) {
-			System.out.println("7: " + e.getMessage());
 			txt.setText("Unable to connect: " + e.getMessage());
 
 			e.printStackTrace();
@@ -176,10 +128,6 @@ public class ChatScreen extends IchiguScreen {
 		super.draw();
 		txt.draw();
 		btn.draw();
-	}
-	
-	public static final class NettyOptionsBuilder extends DefaultOptionsBuilder {
-		
 	}
 	
 	public static final class Data {
